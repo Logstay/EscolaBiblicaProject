3c2c4ff3ac92c8646af9cb324f5d731e7703eb29
logstay <kaiquepng@gmail.com>
2020-03-19 22:13:32 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ApplicationProvider, Layout, Button } from '@ui-kitten/components';
+import { mapping, dark as darkTheme } from '@eva-design/eva';
+import { default as appTheme } from './src/assets/custom-theme'; // <-- Import app theme
+
+const theme = { ...darkTheme, ...appTheme };
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Button>HOME</Button>
+  </Layout>
+);
+
+const App = () => (
+  <ApplicationProvider mapping={mapping} theme={theme}>
+    <HomeScreen/>
+  </ApplicationProvider>
+);
+
+export default App;
